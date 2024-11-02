67e0e5e9b28e1935e19466b66c6585ff0e42bd8b
YaexMiko <aloksinghmains756@gmail.com>
2024-11-02 23:43:26 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -9,7 +9,7 @@ from helper_func import encode, get_message_id
 async def batch(client: Client, message: Message):
     while True:
         try:
-            first_message = await client.ask(text = "Forward the First Message from DB Channel ⏩ (with Quotes)..\n\nor Send the DB Channel Post Link\nUse /sbatch for stopping.", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            first_message = await client.ask(text = "𝙵𝚘𝚛𝚠𝚊𝚛𝚍 𝚃𝚑𝚎 𝙵𝚒𝚛𝚜𝚝 𝙼𝚎𝚜𝚜𝚊𝚐𝚎 𝙵𝚛𝚘𝚖 𝙳𝙱 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 ⏩ (𝚠𝚒𝚝𝚑 𝚀𝚞𝚘𝚝𝚎𝚜)..\n\n𝙾𝚛 𝚂𝚎𝚗𝚍 𝚃𝚑𝚎 𝙳𝙱 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 𝙿𝚘𝚜𝚝 𝙻𝚒𝚗𝚔\n𝚄𝚜𝚎 /sbatch 𝙵𝚘𝚛 𝚂𝚝𝚘𝚙𝚙𝚒𝚗𝚐.", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except Exception as e:
             print(e)
             return
@@ -20,12 +20,12 @@ async def batch(client: Client, message: Message):
         if f_msg_id:
             break
         else:
-            await first_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote = True)
+            await first_message.reply("❌ 𝙴𝚛𝚛𝚘𝚛\n\n𝚃𝚑𝚒𝚜 𝙵𝚘𝚛𝚠𝚊𝚛𝚍𝚎𝚍 𝙿𝚘𝚜𝚝 𝙸𝚜 𝙽𝚘𝚝 𝙵𝚛𝚘𝚖 𝙼𝚢 𝙳𝙱 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 𝙾𝚛 𝚃𝚑𝚒𝚜 𝙻𝚒𝚗𝚔 𝙸𝚜 𝚃𝚊𝚔𝚎𝚗 𝙵𝚛𝚘𝚖 𝙳𝙱 𝙲𝚑𝚊𝚗𝚗𝚎𝚕.", quote = True)
             continue
     while True:
         try:
-            second_message = await client.ask(text = "Forward the Last Message from DB Channel ⏩ (with Quotes)..\nor Send the DB Channel Post link\nUse /sbatch for stopping.", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
-        except:
+            second_message = await client.ask(text = "𝙵𝚘𝚛𝚠𝚊𝚛𝚍 𝚃𝚑𝚎 𝙻𝚊𝚜𝚝 𝙼𝚎𝚜𝚜𝚊𝚐𝚎 𝙵𝚛𝚘𝚖 𝙳𝙱 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 ⏩ (𝚠𝚒𝚝𝚑 𝚀𝚞𝚘𝚝𝚎𝚜)..\n𝙾𝚛 𝚂𝚎𝚗𝚍 𝚃𝚑𝚎 𝙳𝙱 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 𝙿𝚘𝚜𝚝 𝙻𝚒𝚗𝚔\n𝚄𝚜𝚎 /sbatch 𝙵𝚘𝚛 𝚂𝚝𝚘𝚙𝚙𝚒𝚗𝚐.", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+        except:yb 
             return
         if second_message.text == "/sbatch":
             return
@@ -33,14 +33,14 @@ async def batch(client: Client, message: Message):
         if s_msg_id:
             break
         else:
-            await second_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote = True)
+            await second_message.reply("❌ 𝙴𝚛𝚛𝚘𝚛\n\n𝚃𝚑𝚒𝚜 𝙵𝚘𝚛𝚠𝚊𝚛𝚍𝚎𝚍 𝙿𝚘𝚜𝚝 𝙸𝚜 𝙽𝚘𝚝 𝙵𝚛𝚘𝚖 𝙼𝚢 𝙳𝙱 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 𝙾𝚛 𝚃𝚑𝚒𝚜 𝙻𝚒𝚗𝚔 𝙸𝚜 𝚃𝚊𝚔𝚎𝚗 𝙵𝚛𝚘𝚖 𝙳𝙱 𝙲𝚑𝚊𝚗𝚗𝚎𝚕", quote = True)
             continue
         
     string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
     base64_string = await encode(string)
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>🧑‍💻 Here is your code : \n<code>{base64_string}</code></b>\n\n<b>🔗 Here is your link :</b>\n{link}", quote=True, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 𝚂𝚑𝚊𝚛𝚎 𝚄𝚁𝙻", url=f'https://telegram.me/share/url?url={link}')]])
+    await second_message.reply_text(f"<b>🧑‍💻 𝙷𝚎𝚛𝚎 𝙸𝚜 𝚈𝚘𝚞𝚛 𝙲𝚘𝚍𝚎 : \n<code>{base64_string}</code></b>\n\n<b>📤𝙷𝚎𝚛𝚎 𝙸𝚜 𝚈𝚘𝚞𝚛 𝙳𝚘𝚠𝚗𝚕𝚘𝚊𝚍 𝙻𝚒𝚗𝚔📥 :</b>\n{link}", quote=True, reply_markup=reply_markup)
 
 
 
@@ -48,7 +48,7 @@ async def batch(client: Client, message: Message):
 async def link_generator(client: Client, message: Message):
     while True:
         try:
-            channel_message = await client.ask(text = "Forward Message from the DB Channel ⏩ (with Quotes)..\nor Send the DB Channel Post link\nType /sgen for stopping.", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
+            channel_message = await client.ask(text = "𝙵𝚘𝚛𝚠𝚊𝚛𝚍 𝙼𝚎𝚜𝚜𝚊𝚐𝚎 𝙵𝚘𝚛𝚖 𝚃𝚑𝚎 𝙳𝙱 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 ⏩ (𝚠𝚒𝚝𝚑 𝚀𝚞𝚘𝚝𝚎𝚜)..\n𝙾𝚛 𝚂𝚎𝚗𝚍 𝚃𝚑𝚎 𝙳𝙱 𝙲𝚑𝚊𝚗𝚗𝚎𝚕 𝙿𝚘𝚜𝚝 𝙻𝚒𝚗𝚔\n𝚃𝚢𝚙𝚎 /sgen 𝙵𝚘𝚛 𝚂𝚝𝚘𝚙𝚙𝚒𝚗𝚐.", chat_id = message.from_user.id, filters=(filters.forwarded | (filters.text & ~filters.forwarded)), timeout=60)
         except Exception:
             return
         if channel_message.text == "/sgen":
@@ -57,9 +57,9 @@ async def link_generator(client: Client, message: Message):
         if msg_id:
             break
         else:
-            await channel_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote = True)
+            await channel_message.reply("❌ 𝙴𝚛𝚛𝚘𝚛\n\nthis Forwarded Post is not from my DB Channel or this Link is not taken from DB Channel", quote = True)
             continue
     base64_string = await encode(f"get-{msg_id * abs(client.db_channel.id)}")
     link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(f"🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await channel_message.reply_text(f"<b>🧑‍💻 Here is your code : \n<code>{base64_string}</code></b>\n\n<b>🔗 Here is your link : </b>\n{link}", quote=True, reply_markup=reply_markup)
+    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton(f"🔁 𝚂𝚑𝚊𝚛𝚎 𝚄𝚁𝙻", url=f'https://telegram.me/share/url?url={link}')]])
+    await channel_message.reply_text(f"<b>🧑‍💻 𝙷𝚎𝚛𝚎 𝙸𝚜 𝚈𝚘𝚞𝚛 𝙲𝚘𝚍𝚎 : \n<code>{base64_string}</code></b>\n\n<b>📤𝙷𝚎𝚛𝚎 𝙸𝚜 𝚈𝚘𝚞𝚛 𝙳𝚘𝚠𝚗𝚕𝚘𝚊𝚍 𝙻𝚒𝚗𝚔📥 : </b>\n{link}", quote=True, reply_markup=reply_markup)
